93c76b98ff49a724ebb8998d9acf00d626c88195
Evan <ehutch7@gmail.com>
2019-08-27 21:02:36 -0400

    got map working, not have to configure it to be dynamic and focal on parks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { MapComponent } from './components/map/map.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    MapComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/map/map.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import {} from "googlemaps";
+@Component({
+    selector: "app-map",
+    templateUrl: "./map.component.html",
+    styleUrls: [],
+})
+export class MapComponent implements OnInit {
+    @ViewChild("mapContainer", { static: true }) mapElement: any;
+    map: google.maps.Map;
+    constructor() {}
+
+    ngOnInit(): void {
+        const mapProperties = {
+            center: new google.maps.LatLng(35.2271, -80.8431),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+        };
+        this.map = new google.maps.Map(
+            this.mapElement.nativeElement,
+            mapProperties
+        );
+    }
+}
